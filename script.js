77b349104a1a45f3a4bf81c6893e53dc7fe650cf
silvanamedeiros <silvanamedeiros67@yahoo.com>
2025-06-25 20:39:06 -0300

    finalizando

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-  //hamburgue//hamburgerButton
+window.revelar = ScrollReveal({reset:true});
+ 
+ revelar.reveal('.efeito-img-topo', {  //estou chamando a classe a qual eu quero atribuir o efeito de scrollReveal
+   duration: 2000,
+   distance: '70px',
+    origin: 'botom'
+ });
+
+ revelar.reveal('.clientes-efeito', {
+   duration: 2000,
+   deistance: '120px',
+    origin: 'right'
+ });
+
+ revelar.reveal('.principais-produtos', {
+   duration: 2000,
+   deistance: '120px',
+   origin: 'left'
+ });
+ 
+ revelar.reveal('.sobre-nos', {
+   duration: 2000,
+   deistance: '120px',
+   origin: 'top'
+ });
+
+
+ 
+ revelar.reveal('.contatos', {
+   duration: 2000,
+   deistance: '120px',
+   origin: 'botom'
+ });
+
+ 
+ 
  //aki add em evento de sescutar o click do mouse sobre o ícone de hamburguer
 hamburgerButton.addEventListener("click", function() {
    mobileMenu.classList.add("flex");
@@ -7,4 +42,5 @@ hamburgerButton.addEventListener("click", function() {
 //criamos uma função q ao ouvir o click do mouse, ele REMOVE uma class, q está fazendo nossa caixa (div) desaparecer, ou seja, ficará invisível.
 closeButton.addEventListener("click", function(){
 mobileMenu.classList.remove("flex");
-})
+});
+
